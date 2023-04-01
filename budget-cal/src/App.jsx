e8ed5918eb12903c10591422dcef924db24d7cf7
tharindug99 <tngodage@std.appsc.sab.ac.lk>
2023-04-01 09:25:25 +0530

    some changes

--- budget-cal/src/App.jsx
+++ budget-cal/src/App.jsx
@@ -1,12 +1,37 @@
 import { useState } from 'react'
 import './App.css'
+import Alert from './components/Alert'
+import ExpenseForm from './components/ExpenseForm'
+import ExpenseList from './components/ExpenseList'
+import ExpenseItem from './components/ExpenseItem'
+import { v4 as uuidv4 } from 'uuid';
+uuidv4();
+
+const InitialExpenses = [
+  {id: uuidv4(), charge: "rent", amount: 45000},
+  {id: uuidv4(), charge: "Car Lease", amount: 22000},
+  {id: uuidv4(), charge: "Food Expenses", amount: 10000},
+];
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+const [expenses, setExpenses] = useState(InitialExpenses);
+//console.log(expenses);  
+return (
     <div className="App">
       <h1>Budget Calculator</h1>
+      <Alert/>
+      <main className="app">
+      <ExpenseForm/>
+      <ExpenseList expenses={expenses}/>
+      </main>
+      <h1>
+        total spending : <span className='total'>
+          LKR {expenses.reduce((acc,curr)=>{
+            return acc+= curr.amount;
+          },0)}
+        </span>
+      </h1>
     </div>
   )
 }
--- budget-cal/src/components/ExpenseList.jsx
+++ budget-cal/src/components/ExpenseList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Item from './ExpenseItem'
 const ExpenseList = () => {
   return (
     <div>ExpenseList</div>
